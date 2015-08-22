af955d24a822ac549742d2654df6199a8576d2d8
Ritsuki Goto <kawakawaritsuki.dev@gmail.com>
2015-08-23 01:17:22 +0900

    グループとしてのログイン部分や、ログイン状態を弾くメソッド追加

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ require './models/groups_users.rb'
 require 'json'
 require 'uri'
 
-post '/getlocate' do#取得部分/実装完了
+post '/getlocate' do#メンバーの位置取得部分
   #group_id
 
   result = JSON.parse(request.body.read)
@@ -30,13 +30,12 @@ post '/getlocate' do#取得部分/実装完了
 
 end
 
-post '/getgroup' do#取得部分/実装完了
+post '/getgroup' do#グループのメンバー取得部分
   #user_id
 
   result = JSON.parse(request.body.read)
 
   user = User.where(:user_id => result["user_id"])
-
 
   str = "{\"data\":["
 
@@ -52,7 +51,7 @@ post '/getgroup' do#取得部分/実装完了
 
 end
 
-post '/signup' do#会員登録部分/実装完了
+post '/signup' do#会員登録部分
   #user_id/password/user_name
   result = JSON.parse(request.body.read)
   u = User.where(:user_id => result["user_id"]).first
@@ -72,7 +71,7 @@ post '/signup' do#会員登録部分/実装完了
   end
 end
 
-post '/login' do#ログイン部分/仮実装完了
+post '/login' do#ログイン部分
   #userid/password
   result = JSON.parse(request.body.read)
   gather = User.where(:user_id => result["user_id"]).first
@@ -85,7 +84,27 @@ post '/login' do#ログイン部分/仮実装完了
 
 end
 
-post '/regist' do#緯度経度登録部分/実装完了
+post '/grouplogin' do#グループとしてのログイン部分
+  #user_id/group_id
+  result = JSON.parse(request.body.read)
+  user = User.where(:user_id => result["user_id"]).first
+  user.login_now = result["group_id"]
+  user.save
+  "0"
+
+end
+
+post '/grouplogout' do#グループとしてのログアウト部分
+  #user_id
+  result = JSON.parse(request.body.read)
+  user = User.where(:user_id => result["user_id"]).first
+  user.login_now = ""
+  user.save
+  "0"
+
+end
+
+post '/regist' do#位置登録部分
   #userid/latitude/longitude
   result = JSON.parse(request.body.read)
   u = User.where(:user_id => result["user_id"]).first
@@ -100,7 +119,7 @@ post '/regist' do#緯度経度登録部分/実装完了
 
 end
 
-post '/makegroup' do#グループ作成/実装完了
+post '/makegroup' do#グループ作成
   #group_name/user_id
   result = JSON.parse(request.body.read)
 
@@ -118,20 +137,21 @@ post '/makegroup' do#グループ作成/実装完了
   "#{newgroupid}"
 end
 
-post '/ingroup' do#グループに入る/実装完了
+post '/ingroup' do#グループ加入
   #groupid/userid
   #一応ユーザーが存在するかどうかと、
   #重複していないかを確認
+  #エラー処理適当すぎワロタｗ
   result = JSON.parse(request.body.read)
 
   gu = GroupsUser.new
   gu.group_id = Group.where(:group_id => result["group_id"]).first.id
   gu.user_id = User.where(:user_id => result["user_id"]).first.id
   gu.save
-
+  "0"
 end
 
-post '/outgroup' do#グループから出る/実装完了
+post '/outgroup' do#グループ退出
   #group_id/userid
   result = JSON.parse(request.body.read)
   user = User.where(:user_id => result["user_id"]).first
@@ -150,5 +170,25 @@ post '/outgroup' do#グループから出る/実装完了
   else
     "1"
   end
+
+end
+
+post '/loginstate' do#メンバーのログイン状態取得
+  #group_id
+
+  result = JSON.parse(request.body.read)
+  u = Group.where(:group_id => result["group_id"])
+
+  str = "{\"data\":["
+
+  u.each do |g|
+    users = g.user
+    users.each do |user|
+      str = str + "{\"user_id\":\"#{user.user_id}\",\"user_name\":\"#{user.user_name}\",\"login_now\":\"#{user.login_now}\"},"
+    end
+  end
+
+  str = str[0,str.length-1] + "]}"
+  "#{str}"
 
 end
